3c8ba9545f9f4b0e6c9affcdb0a2c46fbf737d97
weiqlu <weiqlu@vt.edu>
2024-07-06 03:26:25 -0400

    changes for deployment

--- server/server.js
+++ server/server.js
@@ -275,10 +275,10 @@ app.put("/password/:username", async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "client/dist", "index.html"));
 });
 
 const port = process.env.PORT || 5000;
